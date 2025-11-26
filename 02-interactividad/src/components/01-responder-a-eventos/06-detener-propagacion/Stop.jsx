8d325c9294d9185e0c0f6a75dc8620281d0e46a2
bryan-ignacio <bryan-ignacio@outlook.com>
2025-11-25 22:39:41 -0600

    terminado stopPropagation

--- 02-interactividad/src/components/01-responder-a-eventos/06-detener-propagacion/Stop.jsx
+++ 02-interactividad/src/components/01-responder-a-eventos/06-detener-propagacion/Stop.jsx
@@ -1,3 +1,40 @@
 export default function Stop() {
-  return <></>;
+  return (
+    <>
+      <ToolBar />
+    </>
+  );
+}
+
+function ToolBar() {
+  function handleSubirClick() {
+    alert("Subiendo archivo!");
+  }
+
+  function handleDescargarClick() {
+    alert("Descargando archivo!");
+  }
+
+  return (
+    <>
+      <div style={{ backgroundColor: "gray" }}>
+        <Button onSmash={handleSubirClick}>Subir</Button>
+        <br />
+        <Button onSmash={handleDescargarClick}>Descargar</Button>
+      </div>
+    </>
+  );
+}
+
+function Button({ onSmash, children }) {
+  function handleClick(e) {
+    e.stopPropagation();
+    onSmash();
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>{children}</button>
+    </>
+  );
 }
--- 02-interactividad/src/main.jsx
+++ 02-interactividad/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import App from "./components/01-responder-a-eventos/06-detener-propagacion/App";
+import Stop from "./components/01-responder-a-eventos/06-detener-propagacion/Stop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Stop />
   </StrictMode>
 );
