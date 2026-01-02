a9a838aa319163bd5df9524b5489fa28e80f229a
bryan-ignacio <bryan-ignacio@outlook.com>
2026-01-02 16:00:19 -0600

    actualizar objeto

---
+++ 02-interactividad/src/components/03-actualizar-obj-en-estado/01-actualizar-objeto/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>Actualizar Objetos en el Estado</h1>
+      <MovingDot />
+    </>
+  );
+}
+
+function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  return (
+    <div
+      onPointerMove={(e) => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      }}
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "red",
+          borderRadius: "50%",
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          left: -10,
+          top: -10,
+          width: 20,
+          height: 20,
+        }}
+      />
+    </div>
+  );
+}
--- 02-interactividad/src/main.jsx
+++ 02-interactividad/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import RequestTracker from "./components/02-el-estado/06-cola-actualizaciones-del-estado/02-desafios/Desafio1";
+import App from "./components/03-actualizar-obj-en-estado/01-actualizar-objeto/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RequestTracker />
+    <App />
   </StrictMode>
 );
