c60b1e8cf2153c6a0884329eb1745f857488bbf2
bryan-ignacio <bryan-ignacio@outlook.com>
2026-01-02 19:25:01 -0600

    actualizar objeto anidado.

--- 02-interactividad/src/components/03-actualizar-obj-en-estado/03-actualizar-objeto-anidado/App.jsx
+++ 02-interactividad/src/components/03-actualizar-obj-en-estado/03-actualizar-objeto-anidado/App.jsx
@@ -1,3 +1,76 @@
+import { useState } from "react";
+
+/*
+    Actualizar una variable de estado que sea un objeto anidado(con varios objetos)
+*/
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Actualizar Objeto Anidado</h1>
+      <Form />
+    </>
+  );
+}
+
+function Form() {
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Nana azul",
+      city: "Hamburgo",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
+
+  function handleChange(e) {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value,
+      artwork: {
+        ...person.artwork,
+        [e.target.name]: e.target.value,
+      },
+    });
+  }
+
+  return (
+    <>
+      <label>Nombre: </label>
+      <input
+        name="name"
+        value={person.name}
+        type="text"
+        onChange={handleChange}
+      />
+      <br />
+      <label>Titulo: </label>
+      <input
+        name="title"
+        value={person.artwork.title}
+        type="text"
+        onChange={handleChange}
+      />
+      <br />
+      <label>Ciudad: </label>
+      <input
+        name="city"
+        value={person.artwork.city}
+        type="text"
+        onChange={handleChange}
+      />
+      <br />
+      <label>Imagen: </label>
+      <input
+        name="image"
+        value={person.artwork.image}
+        type="text"
+        onChange={handleChange}
+      />
+      <p>
+        {person.artwork.title} por {person.name} <br />
+        (situada en {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
+  );
 }
