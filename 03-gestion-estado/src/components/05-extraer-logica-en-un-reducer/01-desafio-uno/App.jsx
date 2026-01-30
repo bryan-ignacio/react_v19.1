886c82fa65b5293937e8f46199676d5af957f729
bryan-ignacio <bryan-ignacio@outlook.com>
2026-01-29 23:20:31 -0600

    reducer

--- 03-gestion-estado/src/components/05-extraer-logica-en-un-reducer/01-desafio-uno/App.jsx
+++ 03-gestion-estado/src/components/05-extraer-logica-en-un-reducer/01-desafio-uno/App.jsx
@@ -9,9 +9,18 @@ const initialState = {
 };
 
 export default function Messenger() {
+  // declaro el hook useReducer(reducer, estado)
   const [state, dispatch] = useReducer(messengerReducer, initialState);
+  // declaro una cte que contendra el estado y accedo al mensaje.
   const message = state.message;
+  // declaro una cte contacto que contendra el contacto encontrado apartir del id y el del id del estado.
   const contact = contacts.find((c) => c.id === state.selectedId);
+
+  /*
+   que retorna el jsx.
+   - un Componente ContactList (contenra los contactos, el id seleccionado, dispatch el despachador de accion)
+   - un Componente Chat (las propiedades key, message, contact, dispatch)
+   */
   return (
     <div>
       <ContactList
