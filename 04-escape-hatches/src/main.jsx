94131266c5c82fe183250dae4dab378215fc0837
bryan-ignacio <bryan-ignacio@outlook.com>
2026-02-21 22:16:52 -0600

    esca hatches: first]

--- 04-escape-hatches/src/main.jsx
+++ 04-escape-hatches/src/main.jsx
@@ -2,16 +2,4 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./01-hook-ref/App.css";
 
-import ConEstado from "./01-hook-ref/03-cuando-usar/App";
-import ConRef from "./01-hook-ref/03-cuando-usar/Ref";
-//import App from "./01-hook-ref/04-desafio-uno/App";
-//import App from "./01-hook-ref/05-desafio-dos/App";
-//import App from "./01-hook-ref/06-desafio-tres/App";
-// import App from "./01-hook-ref/07-desafio-cuatro/App";
-import Stopwatch from "./01-hook-ref/02-cronometro/App";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Stopwatch />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
